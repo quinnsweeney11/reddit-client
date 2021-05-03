279135c16010b8099bc5c243ac402fd86570858c
Quinn Sweeney <quinnsweeney11@gmail.com>
2021-05-03 12:42:37 -0600

    VIDEO PLAYER WORKS YAYYY

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -1,36 +1,48 @@
 import React from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import "./post.css";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
-import {PicCarousel} from '../../utils/picCarousel/PicCarousel';
+import { PicCarousel } from "../../utils/picCarousel/PicCarousel";
+import "../../../node_modules/video-react/dist/video-react.css";
+import ReactHlsPlayer from "react-hls-player";
 
-import {
-    setGallery
-} from './postSlice'
+import { setGallery } from "./postSlice";
 
 //Function that we use to format the text posts.
 function htmlDecode(input) {
-
     var doc = new DOMParser().parseFromString(input, "text/html");
-    
-    return doc.documentElement.textContent;   
+
+    return doc.documentElement.textContent;
 }
 
 export function Post(props) {
     const dispatch = useDispatch();
 
-    function formatCarousel(pics) {
-        const handleDragStart = (e) => e.preventDefault();
-        const output = [];
-        pics.items.forEach(img => {
-            const picLink = `https://i.redd.it/${img.media_id}.jpg`
-            output.push(<img style={{width: '60%'}} src={picLink} alt="Carousel content" onDragStart={handleDragStart} />)
-        })
-        return output;
+    if (props.data.secure_media) {
+        console.log(props.data.secure_media.reddit_video.fallback_url);
+        return (
+            <div className="post post_video">
+                <div className="info">
+                    <div className="info-item points">
+                        <ArrowUpwardIcon />
+                        <p>{props.data.score}</p>
+                    </div>
+                    <br />
+                    <div className="info-item children">
+                        <ChatBubbleIcon />
+                        <p>{props.data.num_comments}</p>
+                    </div>
+                </div>
+                <div className="contents">
+                    <h2>{props.data.title}</h2>
+                    <ReactHlsPlayer src={props.data.secure_media.reddit_video.hls_url} autoplay={false} controls={true} class="video-container"/>
+                </div>
+            </div>
+        );
     }
 
-    if(props.data.post_hint === 'image'){
+    if (props.data.post_hint === "image") {
         return (
             <div className="post post_image">
                 <div className="info">
@@ -46,16 +58,19 @@ export function Post(props) {
                 </div>
                 <div className="contents">
                     <h2>{props.data.title}</h2>
-                    <img className="image" alt="Post Content" src={props.data.url} />
+                    <img
+                        className="image"
+                        alt="Post Content"
+                        src={props.data.url}
+                    />
                 </div>
             </div>
         );
     }
 
-    if(props.data.gallery_data){
-        // console.log(formatCarousel(props.data.gallery_data))
+    if (props.data.gallery_data) {
         return (
-            <div className="post">
+            <div className="post post_carousel">
                 <div className="info">
                     <div className="info-item points">
                         <ArrowUpwardIcon />
@@ -69,8 +84,10 @@ export function Post(props) {
                 </div>
                 <div className="contents">
                     <h2>{props.data.title}</h2>
-                    <PicCarousel className="carousel" pics={props.data.gallery_data.items}/>
-                    {console.log(props.data.gallery_data)}
+                    <PicCarousel
+                        className="carousel"
+                        pics={props.data.gallery_data.items}
+                    />
                 </div>
             </div>
         );
@@ -92,12 +109,11 @@ export function Post(props) {
                 </div>
                 <div className="contents">
                     <h2>{props.data.title}</h2>
-                    
                 </div>
             </div>
         );
-    }   
-        
+    }
+
     return (
         <div className="post">
             <div className="info">
@@ -113,7 +129,12 @@ export function Post(props) {
             </div>
             <div className="contents">
                 <h4>{props.data.title}</h4>
-                <div className="selftext" dangerouslySetInnerHTML={{__html: htmlDecode(props.data.selftext_html)}}></div>
+                <div
+                    className="selftext"
+                    dangerouslySetInnerHTML={{
+                        __html: htmlDecode(props.data.selftext_html),
+                    }}
+                ></div>
             </div>
         </div>
     );
--- src/utils/picCarousel/PicCarousel.js
+++ src/utils/picCarousel/PicCarousel.js
@@ -9,7 +9,6 @@ import './picCarousel.css';
 
 export function PicCarousel(props) {
     const [currentPic, setCurrentPic] = useState(0);
-    console.log(props)
     function nextPic(){
         setCurrentPic(currentPic + 1);
     }
@@ -20,11 +19,11 @@ export function PicCarousel(props) {
 
     return(
         <div className="PicCarousel">
-            <img className="shown-img" src={`https://i.redd.it/${props.pics[currentPic].media_id}.jpg`} alt="Carousel" />
             <div className="controls">
                 {currentPic === 0 ? <button disabled>🚫</button>: <button onClick={() => previousPic()}>👈</button>}
                 {currentPic === props.pics.length - 1 ? <button disabled>🚫</button> : <button onClick={() => nextPic()}>👉</button>}
             </div>
+            <img className="shown-img" src={`https://i.redd.it/${props.pics[currentPic].media_id}.jpg`} alt="Carousel" />
         </div>
     )
 }
