5ac94399bad8877f54e0225ae8d8dbaefc12f6f6
Quinn Sweeney <quinnsweeney11@gmail.com>
2021-05-24 11:22:44 -0600

    link posts work now, also added a button on each post to view the post in a new tab

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -2,193 +2,340 @@ import React from "react";
 import "./post.css";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
 import { PicCarousel } from "../../utils/picCarousel/PicCarousel";
 import "../../../node_modules/video-react/dist/video-react.css";
 import ReactHlsPlayer from "react-hls-player";
-import { toggleShowPosts } from '../../containers/postContainer/postContainerSlice'
-import { useDispatch } from 'react-redux'
+import { toggleShowPosts } from "../../containers/postContainer/postContainerSlice";
+import { useDispatch } from "react-redux";
 
 //Function that we use to format the text posts.
 function htmlDecode(input) {
-    var doc = new DOMParser().parseFromString(input, "text/html");
+  var doc = new DOMParser().parseFromString(input, "text/html");
 
-    return doc.documentElement.textContent;
+  return doc.documentElement.textContent;
 }
 
 export function Post(props) {
-    const dispatch = useDispatch();
-    if(props.newWarn === true) {
-        return (
-            <div className="post post_newWarn">
-                <div className="contents">
-                    <h1 className="warning">WARNING: You are browsing using the 'new' filter. These posts are very unmoderated, and can contain innapropriate content. Proceed at your own risk.</h1>
-                    <button onClick={() => dispatch(toggleShowPosts(true))}>I understand, let me in!</button>
-                </div>
-            </div>
-        )
-    }
-
-    if (props.data.secure_media && props.data.secure_media.reddit_video) {
-        return (
-            <div className="post post_video">
-                <div className="info">
-                    <div className="info-item points">
-                        <ArrowUpwardIcon />
-                        <p>{props.data.score}</p>
-                    </div>
-                    <br />
-                    <div className="info-item children">
-                        <ChatBubbleIcon />
-                        <p>{props.data.num_comments}</p>
-                    </div>
-                </div>
-                <div className="contents">
-                    <div className="info-item metadata">
-                        <p><span className="rslash">r/</span>{props.data.subreddit}</p>
-                        <p><span className="uslash">u/</span>{props.data.author}</p>
-                    </div>
-                    <h2 className="title-text">{props.data.title}</h2>
-                    <ReactHlsPlayer src={props.data.secure_media.reddit_video.hls_url} autoplay={false} controls={true} className="video-container"/>
-                </div>
-            </div>
-        );
-    } else if(props.data.secure_media && props.data.secure_media.oembed) {
-        return (
-            <div className="post post_gif">
-                <div className="info">
-                    <div className="info-item points">
-                        <ArrowUpwardIcon />
-                        <p>{props.data.score}</p>
-                    </div>
-                    <br />
-                    <div className="info-item children">
-                        <ChatBubbleIcon />
-                        <p>{props.data.num_comments}</p>
-                    </div>
-                </div>
-                <div className="contents">
-                    <div className="info-item metadata">
-                        <p><span className="rslash">r/</span>{props.data.subreddit}</p>
-                        <p><span className="uslash">u/</span>{props.data.author}</p>
-                    </div>
-                    <h2>{props.data.title}</h2>
-                    <div className="embedded_media" dangerouslySetInnerHTML={{__html: htmlDecode(props.data.secure_media.oembed.html)}}>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
-    if (props.data.post_hint === "image") {
-        return (
-            <div className="post post_image">
-                <div className="info">
-                    <div className="info-item points">
-                        <ArrowUpwardIcon />
-                        <p>{props.data.score}</p>
-                    </div>
-                    <br />
-                    <div className="info-item children">
-                        <ChatBubbleIcon />
-                        <p>{props.data.num_comments}</p>
-                    </div>
-                </div>
-                <div className="contents">
-                    <div className="info-item metadata">
-                        <p><span className="rslash">r/</span>{props.data.subreddit}</p>
-                        <p><span className="uslash">u/</span>{props.data.author}</p>
-                    </div>
-                    <h2>{props.data.title}</h2>
-                    <img
-                        className="image"
-                        alt="Post Content"
-                        src={props.data.url}
-                    />
-                </div>
-            </div>
-        );
-    }
-
-    if (props.data.gallery_data) {
-        return (
-            <div className="post post_carousel">
-                <div className="info">
-                    <div className="info-item points">
-                        <ArrowUpwardIcon />
-                        <p>{props.data.score}</p>
-                    </div>
-                    <br />
-                    <div className="info-item children">
-                        <ChatBubbleIcon />
-                        <p>{props.data.num_comments}</p>
-                    </div>
-                </div>
-                <div className="contents">
-                    <div className="info-item metadata">
-                        <p><span className="rslash">r/</span>{props.data.subreddit}</p>
-                        <p><span className="uslash">u/</span>{props.data.author}</p>
-                    </div>
-                    <h2>{props.data.title}</h2>
-                    <PicCarousel
-                        className="carousel"
-                        pics={props.data.gallery_data.items}
-                    />
-                </div>
-            </div>
-        );
-    }
-
-    if (props.data.selftext === "") {
-        return (
-            <div className="post">
-                <div className="info">
-                    <div className="info-item points">
-                        <ArrowUpwardIcon />
-                        <p>{props.data.score}</p>
-                    </div>
-                    <br />
-                    <div className="info-item children">
-                        <ChatBubbleIcon />
-                        <p>{props.data.num_comments}</p>
-                    </div>
-                </div>
-                <div className="contents">
-                    <div className="info-item metadata">
-                        <p><span className="rslash">r/</span>{props.data.subreddit}</p>
-                        <p><span className="uslash">u/</span>{props.data.author}</p>
-                    </div>
-                    <h2>{props.data.title}</h2>
-                </div>
-            </div>
-        );
-    }
-
+  const dispatch = useDispatch();
+  if (props.newWarn === true) {
     return (
-        <div className="post">
-            <div className="info">
-                <div className="info-item points">
-                    <ArrowUpwardIcon />
-                    <p>{props.data.score}</p>
-                </div>
-                <br />
-                <div className="info-item children">
-                    <ChatBubbleIcon />
-                    <p>{props.data.num_comments}</p>
-                </div>
-            </div>
-            <div className="contents">
-                <div className="info-item metadata">
-                    <p><span className="rslash">r/</span>{props.data.subreddit}</p>
-                    <p><span className="uslash">u/</span>{props.data.author}</p>
-                </div>
-                <h4>{props.data.title}</h4>
-                <div
-                    className="selftext"
-                    dangerouslySetInnerHTML={{
-                        __html: htmlDecode(props.data.selftext_html),
-                    }}
-                ></div>
-            </div>
+      <div className="post post_newWarn">
+        <div className="contents">
+          <h1 className="warning">
+            WARNING: You are browsing using the 'new' filter. These posts are
+            very unmoderated, and can contain innapropriate content. Proceed at
+            your own risk.
+          </h1>
+          <button onClick={() => dispatch(toggleShowPosts(true))}>
+            I understand, let me in!
+          </button>
         </div>
+      </div>
     );
+  }
+
+  if (props.data.secure_media && props.data.secure_media.reddit_video) {
+    return (
+      <div className="post post_video">
+        <div className="info">
+          <div className="info-item points">
+            <ArrowUpwardIcon />
+            <p>{props.data.score}</p>
+          </div>
+          <br />
+          <div className="info-item children">
+            <ChatBubbleIcon />
+            <p>{props.data.num_comments}</p>
+          </div>
+          <div className="info-item open">
+            <a
+              href={`https://www.reddit.com${props.data.permalink}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <OpenInNewIcon />
+            </a>
+          </div>
+        </div>
+        <div className="contents">
+          <div className="info-item metadata">
+            <p>
+              <span className="rslash">r/</span>
+              {props.data.subreddit}
+            </p>
+            <p>
+              <span className="uslash">u/</span>
+              {props.data.author}
+            </p>
+          </div>
+          <h2 className="title-text">{props.data.title}</h2>
+          <ReactHlsPlayer
+            src={props.data.secure_media.reddit_video.hls_url}
+            autoplay={false}
+            controls={true}
+            className="video-container"
+          />
+        </div>
+      </div>
+    );
+  } else if (props.data.secure_media && props.data.secure_media.oembed) {
+    return (
+      <div className="post post_gif">
+        <div className="info">
+          <div className="info-item points">
+            <ArrowUpwardIcon />
+            <p>{props.data.score}</p>
+          </div>
+          <br />
+          <div className="info-item children">
+            <ChatBubbleIcon />
+            <p>{props.data.num_comments}</p>
+          </div>
+          <div className="info-item open">
+            <a
+              href={`https://www.reddit.com${props.data.permalink}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <OpenInNewIcon />
+            </a>
+          </div>
+        </div>
+        <div className="contents">
+          <div className="info-item metadata">
+            <p>
+              <span className="rslash">r/</span>
+              {props.data.subreddit}
+            </p>
+            <p>
+              <span className="uslash">u/</span>
+              {props.data.author}
+            </p>
+          </div>
+          <h2>{props.data.title}</h2>
+          <div
+            className="embedded_media"
+            dangerouslySetInnerHTML={{
+              __html: htmlDecode(props.data.secure_media.oembed.html),
+            }}
+          ></div>
+        </div>
+      </div>
+    );
+  }
+
+  if (props.data.post_hint === "image") {
+    return (
+      <div className="post post_image">
+        <div className="info">
+          <div className="info-item points">
+            <ArrowUpwardIcon />
+            <p>{props.data.score}</p>
+          </div>
+          <br />
+          <div className="info-item children">
+            <ChatBubbleIcon />
+            <p>{props.data.num_comments}</p>
+          </div>
+          <div className="info-item open">
+            <a
+              href={`https://www.reddit.com${props.data.permalink}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <OpenInNewIcon />
+            </a>
+          </div>
+        </div>
+        <div className="contents">
+          <div className="info-item metadata">
+            <p>
+              <span className="rslash">r/</span>
+              {props.data.subreddit}
+            </p>
+            <p>
+              <span className="uslash">u/</span>
+              {props.data.author}
+            </p>
+          </div>
+          <h2>{props.data.title}</h2>
+          <img className="image" alt="Post Content" src={props.data.url} />
+        </div>
+      </div>
+    );
+  }
+
+  if (props.data.gallery_data) {
+    return (
+      <div className="post post_carousel">
+        <div className="info">
+          <div className="info-item points">
+            <ArrowUpwardIcon />
+            <p>{props.data.score}</p>
+          </div>
+          <br />
+          <div className="info-item children">
+            <ChatBubbleIcon />
+            <p>{props.data.num_comments}</p>
+          </div>
+          <div className="info-item open">
+            <a
+              href={`https://www.reddit.com${props.data.permalink}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <OpenInNewIcon />
+            </a>
+          </div>
+        </div>
+        <div className="contents">
+          <div className="info-item metadata">
+            <p>
+              <span className="rslash">r/</span>
+              {props.data.subreddit}
+            </p>
+            <p>
+              <span className="uslash">u/</span>
+              {props.data.author}
+            </p>
+          </div>
+          <h2>{props.data.title}</h2>
+          <PicCarousel
+            className="carousel"
+            pics={props.data.gallery_data.items}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  if (props.data.selftext === "") {
+    if (props.data.domain.startsWith("self.")) {
+      return (
+        <div className="post">
+          <div className="info">
+            <div className="info-item points">
+              <ArrowUpwardIcon />
+              <p>{props.data.score}</p>
+            </div>
+            <br />
+            <div className="info-item children">
+              <ChatBubbleIcon />
+              <p>{props.data.num_comments}</p>
+            </div>
+            <div className="info-item open">
+              <a
+                href={`https://www.reddit.com${props.data.permalink}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <OpenInNewIcon />
+              </a>
+            </div>
+          </div>
+          <div className="contents">
+            <div className="info-item metadata">
+              <p>
+                <span className="rslash">r/</span>
+                {props.data.subreddit}
+              </p>
+              <p>
+                <span className="uslash">u/</span>
+                {props.data.author}
+              </p>
+            </div>
+            <h2>{props.data.title}</h2>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="post">
+          <div className="info">
+            <div className="info-item points">
+              <ArrowUpwardIcon />
+              <p>{props.data.score}</p>
+            </div>
+            <br />
+            <div className="info-item children">
+              <ChatBubbleIcon />
+              <p>{props.data.num_comments}</p>
+            </div>
+            <div className="info-item open">
+              <a
+                href={`https://www.reddit.com${props.data.permalink}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <OpenInNewIcon />
+              </a>
+            </div>
+          </div>
+          <div className="contents">
+            <div className="info-item metadata">
+              <p>
+                <span className="rslash">r/</span>
+                {props.data.subreddit}
+              </p>
+              <p>
+                <span className="uslash">u/</span>
+                {props.data.author}
+              </p>
+            </div>
+            <h2>{props.data.title}</h2>
+            <h4>
+              <a href={props.data.url} target="_blank" rel="noreferrer">
+                {props.data.url}
+              </a>
+            </h4>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className="post">
+      <div className="info">
+        <div className="info-item points">
+          <ArrowUpwardIcon />
+          <p>{props.data.score}</p>
+        </div>
+        <br />
+        <div className="info-item children">
+          <ChatBubbleIcon />
+          <p>{props.data.num_comments}</p>
+        </div>
+        <div className="info-item open">
+          <a
+            href={`https://www.reddit.com${props.data.permalink}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <OpenInNewIcon />
+          </a>
+        </div>
+      </div>
+      <div className="contents">
+        <div className="info-item metadata">
+          <p>
+            <span className="rslash">r/</span>
+            {props.data.subreddit}
+          </p>
+          <p>
+            <span className="uslash">u/</span>
+            {props.data.author}
+          </p>
+        </div>
+        <h4>{props.data.title}</h4>
+        <div
+          className="selftext"
+          dangerouslySetInnerHTML={{
+            __html: htmlDecode(props.data.selftext_html),
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 }
